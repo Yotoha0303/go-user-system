90ea17044d992a0a66b2a36182df4144f215f76c
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2026-04-30 11:54:50 +0800

    update model struct file

--- model/user.go
+++ model/user.go
@@ -8,15 +8,16 @@ const (
 )
 
 type User struct {
-	ID           uint64 `gorm:"primaryKey;autoIncrement"`
-	Username     string `gorm:"size:64;not null;uniqueIndex"`
-	PasswordHash string `gorm:"size:255;not null"`
-	Nickname     string `gorm:"size:64;not null;default:''"`
-	Status       int8   `gorm:"not null;default:1"`
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           int64     `gorm:"primaryKey;autoIncrement" json:"id"`
+	Username     string    `gorm:"size:64;not null;uniqueIndex:idx_username" json:"username"`
+	PasswordHash string    `gorm:"size:255;not null" json:"-"`
+	Nickname     string    `gorm:"size:64;not null;default:''" json:"nickname"`
+	Status       int8      `gorm:"not null;default:1" json:"status"`
+	CreatedAt    time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
 
+//GORM 默认会把 User 映射成 users;自定义表名
 func (User) tableName() string {
 	return "users"
 }
